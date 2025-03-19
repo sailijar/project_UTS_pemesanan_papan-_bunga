e9990f4775da1a4beed751c7a228fc3dce54c25d
sailijar <sailijar88@gmail.com>
2025-03-19 14:43:07 +0700

    memperbaruie file web.php

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('admin/dashboard', function () {
+    return view('admin.dashboard');
+})->name('dashboard');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('admin/barang', function () {
+    return view('admin.barang');
+})->name('barang');
+
+Route::get('admin/pemesanan', function () {
+    return view('admin.pemesanan');
+})->name('pesanan');
+
+Route::get('admin/transaksi', function () {
+    return view('admin.transaksi');
+})->name('transaksi');
